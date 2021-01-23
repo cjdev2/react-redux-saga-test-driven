bd5521ce1e770d9099e15afdd79e416fa08a379b
Sean Shubin <sshubin@cj.com>
2021-01-23 13:53:26 -0800

    enhanced debugging for tester

--- src/test-util/connectedTester.js
+++ src/test-util/connectedTester.js
@@ -9,7 +9,7 @@ import userEvent from '@testing-library/user-event'
 import createPromiseTracker from './promise-tracker';
 import {createMemoryHistory} from 'history';
 import * as R from 'ramda';
-import createSagaMonitor from './saga-monitor';
+import util from 'util'
 
 const effectiveStateFor = (model, state) => {
     const accumulateState = (accumulator, lens) => {
@@ -20,16 +20,65 @@ const effectiveStateFor = (model, state) => {
     return R.reduce(accumulateState, {}, R.values(model))
 }
 
+const createHistoryMonitor = handleEvent => {
+    const historyMonitor = ({location, action}) => {
+        const event = {action, pathname: location.pathname, state: location.state}
+        handleEvent(event)
+    }
+    return historyMonitor
+}
+
+const createReduxMonitor = handleEvent => {
+    const reduxMonitor = store => next => event => {
+        handleEvent(event)
+        return next(event)
+    }
+    return reduxMonitor
+}
+
+const createSagaMonitor = handleEvent => {
+    const rootSagaStarted = (options) => {
+        handleEvent({rootSagaStarted: options})
+    }
+    const effectTriggered = (options) => {
+        handleEvent({sagaEffectTriggered: options})
+    }
+    const effectResolved = (effectId, result) => {
+        handleEvent({sagaEffectResolved: {effectId, result}})
+    }
+    const effectRejected = (effectId, error) => {
+        handleEvent({sagaEffectRejected: {effectId, error}})
+    }
+    const effectCancelled = effectId => {
+        handleEvent({sagaEffectCancelled: {effectId}})
+    }
+    const actionDispatched = action => {
+        handleEvent({sagaActionDispatched: {action}})
+    }
+    const sagaMonitor = {
+        rootSagaStarted,
+        effectTriggered,
+        effectResolved,
+        effectRejected,
+        effectCancelled,
+        actionDispatched
+    }
+    return sagaMonitor
+}
+
 const createConnectedTester = ({connected, uri, fetchSpecs = [], initialState}) => {
     const history = createMemoryHistory()
     if (uri) {
         history.push(uri)
         history.go(0)
     }
+    const combinedEvents = []
     const historyEvents = []
-    history.listen(({location, action}) => {
-        historyEvents.push({action, pathname: location.pathname, state: location.state})
-    });
+    const handleHistoryEvent = historyEvent => {
+        historyEvents.push(historyEvent)
+        combinedEvents.push({history: historyEvent})
+    }
+    history.listen(createHistoryMonitor(handleHistoryEvent));
     const {attachTracking, waitForAllPromises} = createPromiseTracker()
     const fetch = createFetchFake(fetchSpecs)
     const untrackedEnvironment = createEnvironment({history, window, fetch})
@@ -38,16 +87,22 @@ const createConnectedTester = ({connected, uri, fetchSpecs = [], initialState}) 
         fetchText: attachTracking('fetchText', untrackedEnvironment.fetchText),
         fetchJson: attachTracking('fetchJson', untrackedEnvironment.fetchJson)
     }
-    const {sagaMonitor} = createSagaMonitor()
+    const sagaEvents = []
+    const handleSagaEvent = sagaEvent => {
+        sagaEvents.push(sagaEvent)
+        combinedEvents.push({saga: sagaEvent})
+    }
+    const sagaMonitor = createSagaMonitor(handleSagaEvent)
     const sagaMiddleware = createSagaMiddleware({sagaMonitor})
     const reduxEvents = []
-    const monitor = store => next => event => {
-        reduxEvents.push(event)
-        return next(event)
+    const handleReduxEvent = reduxEvent => {
+        reduxEvents.push(reduxEvent)
+        combinedEvents.push({redux: reduxEvent})
     }
+    const reduxMonitor = createReduxMonitor(handleReduxEvent)
     const reducer = connected.reducer
     const state = initialState || connected.initialState
-    const store = createStore(reducer, state, applyMiddleware(sagaMiddleware, monitor))
+    const store = createStore(reducer, state, applyMiddleware(sagaMiddleware, reduxMonitor))
     const saga = connected.saga(environment)
     sagaMiddleware.run(saga)
     const waitForEvents = async () => {
@@ -83,20 +138,15 @@ const createConnectedTester = ({connected, uri, fetchSpecs = [], initialState}) 
     }
     const Component = connected.Component
     const rendered = render(<Provider store={store}><Component/></Provider>)
-    const debug = () => {
-        console.log('view')
-        rendered.debug()
-
-        console.log('model')
-        console.log(JSON.stringify(store.getState(), null, 2))
-
-        console.log('redux events')
-        console.log(reduxEvents)
-
-        console.log(`history events (${history.location.pathname})`)
-        console.log(historyEvents)
-    }
     const effectiveState = () => effectiveStateFor(connected.model, store.getState())
+    const debug = () => {
+        rendered.debug()
+        const debugObject = {
+            model: effectiveState(),
+            events: combinedEvents
+        }
+        console.log(util.inspect(debugObject, {depth: null}))
+    }
     return {
         dispatch,
         store,
@@ -109,6 +159,7 @@ const createConnectedTester = ({connected, uri, fetchSpecs = [], initialState}) 
         userClicksElementWithLabelTextWithOptions,
         history,
         historyEvents,
+        sagaEvents,
         effectiveState,
         debug
     }
